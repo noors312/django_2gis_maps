1be7b9bcf5643b80c1fe94e3c117c31f1d095e19
Nurs <jackmovies01@gmail.com>
2018-07-29 20:27:55 +0600

    updates

--- django_2gis_maps/widgets.py
+++ django_2gis_maps/widgets.py
@@ -4,7 +4,7 @@ from django.forms import widgets
 
 class DoubleGisMapsAddressWidget(widgets.TextInput):
     """a widget that will place a google map right after the #id_address field"""
-    template_name = "django_google_maps/widgets/map_widget.html"
+    template_name = "django_2gis_maps/widgets/map_widget.html"
 
     class Media:
         css = {
